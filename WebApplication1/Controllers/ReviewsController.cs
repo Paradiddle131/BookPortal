2862d0f14516ce4a6085eb60b5a72c5bd0a19696
Paradiddle131 <ebhan96@hotmail.com>
2019-04-23 01:51:00 +0300

    RevDB

--- WebApplication1/Controllers/ReviewsController.cs
+++ WebApplication1/Controllers/ReviewsController.cs
@@ -1,47 +1,65 @@
-﻿using System.Linq;
+﻿using DataLibrary.ModelsD;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace BookPortal.Controllers
 {
 	public class ReviewsController : Controller
 	{
-		//private readonly ReviewADO rado = new ReviewADO();
-		//private readonly libraryEntities le = new libraryEntities();
-		private readonly ReviewBookEntity rb = new ReviewBookEntity();
-
-		//private ReviewContext context = new ReviewContext();
+		private readonly RevDBEntities rb = new RevDBEntities();
 
 		// GET: Reviews
 
 		public ActionResult Index(string searching)
 		{
-			//var reviews = from s in le.types
-			//			  select s;
-			return View(rb.Reviews.Where(x => x.Category.Contains(searching) || searching == null).ToList());
+			ViewBag.Message = "Review List";
+
+			List<ReviewModel> data = DataLibrary.BusinessLogic.ReviewProcessor.LoadReviews();
+			List<ReviewModel> reviews = new List<ReviewModel>();
+			foreach (ReviewModel row in data)
+			{
+				reviews.Add(new ReviewModel
+				{
+					Category = row.Category,
+					BookName = row.BookName,
+					ReviewType = row.ReviewType,
+					BookSubject = row.BookSubject,
+					BookAnalysis = row.BookAnalysis,
+					AuthorName = row.AuthorName,
+					OtherBooks = row.OtherBooks
+				});
+			}
+			//return View(rb.Reviews.Where);
+			return View(rb.Reviews.Where(x => x.AuthorName.Contains(searching) || searching == null).ToList());
+			//return View(reviews);
+
+
+
 		}
 
 		//public ActionResult Index()
-		//      {
-		//	//return View(context.Reviews.ToList());
-		//	ViewBag.Message = "Review List";
+		//{
+		//return View(context.Reviews.ToList());
+		//ViewBag.Message = "Review List";
 
-		//	var data = LoadReviews();
-		//	List<ReviewModel> reviews = new List<ReviewModel>();
-		//	foreach (var row in data)
+		//List<ReviewModel> data = DataLibrary.BusinessLogic.ReviewProcessor.LoadReviews();
+		//List<ReviewModel> reviews = new List<ReviewModel>();
+		//foreach (ReviewModel row in data)
+		//{
+		//	reviews.Add(new ReviewModel
 		//	{
-		//		reviews.Add(new ReviewModel
-		//		{
-		//			Category = row.Category,
-		//			BookName = row.BookName,
-		//			ReviewType = row.ReviewType,
-		//			BookSubject = row.BookSubject,
-		//			BookAnalysis = row.BookAnalysis,
-		//			AuthorName = row.AuthorName,
-		//			OtherBooks = row.OtherBooks
-		//		});
-		//	}
+		//		Category = row.Category,
+		//		BookName = row.BookName,
+		//		ReviewType = row.ReviewType,
+		//		BookSubject = row.BookSubject,
+		//		BookAnalysis = row.BookAnalysis,
+		//		AuthorName = row.AuthorName,
+		//		OtherBooks = row.OtherBooks
+		//	});
+		//}
 
-		//	return View(reviews);
+		//return View(reviews);
 		//}
 	}
 }
