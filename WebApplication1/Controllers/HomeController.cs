e73090ee925eb8baf5f6cbf41ee0eeec9e51a73f
Paradiddle131 <ebhan96@hotmail.com>
2019-04-28 13:30:01 +0300

    Search Added
    - Search function added to Reviews page.
    - Old databases removed.
    - New database RevModel.edmx added.
    - DataLibrary class library and its components removed.
    - New review model added.
    - AddReview page removed.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,20 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace BookPortal.Controllers
 {
-    public class HomeController : Controller
-    {
+	public class HomeController : Controller
+	{
 
 
 
-        // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+		//public ActionResult AddReview()
+		//{
+		//	return View();
+		//}
+		//public ActionResult Contact()
+		//{
+		//	ViewBag.Message = "Your contact page.";
+
+		//	return View();
+		//}
+		//public ActionResult Review()
+		//{
+		//	ViewBag.Message = "Your review page.";
+
+		//	return View();
+		//}
+	}
 }
---
+++ WebApplication1/Controllers/ReviewController.cs
@@ -0,0 +1,137 @@
+﻿using BookPortal.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace BookPortal.Controllers
+{
+	public class ReviewController : Controller
+	{
+		private readonly RevDBEntities db = new RevDBEntities();
+
+		// GET: Review
+		[HttpGet]
+		public ActionResult Index()
+		{
+			ViewBag.BookName = "";
+			ViewBag.AuthorName = "";
+			ViewBag.Category = "";
+			IQueryable<Review> reviews = db.Reviews.Include(a => a.AuthorName);
+			return View(db.Reviews.ToList());
+			//return View(reviews.ToList());
+		}
+		[HttpPost]
+		public ActionResult Index(string bookName, string authorName, string category, Review rev)
+		{
+			IEnumerable<Review> reviews = db.Reviews.ToList().Where(r => r.BookName.StartsWith(bookName) && r.AuthorName.StartsWith(authorName) && r.Category.StartsWith(category));
+			return View(reviews);
+		}
+
+		// GET: Review/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Review review = db.Reviews.Find(id);
+			if (review == null)
+			{
+				return HttpNotFound();
+			}
+			return View(review);
+		}
+
+		// GET: Review/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Review/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "ReviewId,Category,BookName,ReviewType,BookSubject,BookAnalysis,AuthorName,OtherBooks")] Review review)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Reviews.Add(review);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(review);
+		}
+
+		// GET: Review/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Review review = db.Reviews.Find(id);
+			if (review == null)
+			{
+				return HttpNotFound();
+			}
+			return View(review);
+		}
+
+		// POST: Review/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "ReviewId,Category,BookName,ReviewType,BookSubject,BookAnalysis,AuthorName,OtherBooks")] Review review)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(review).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(review);
+		}
+
+		// GET: Review/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Review review = db.Reviews.Find(id);
+			if (review == null)
+			{
+				return HttpNotFound();
+			}
+			return View(review);
+		}
+
+		// POST: Review/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Review review = db.Reviews.Find(id);
+			db.Reviews.Remove(review);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
