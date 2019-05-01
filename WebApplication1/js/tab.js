ea462af583da2d8597cf2650c90f18b94131bc85
Tolga <arslanntolgaa@gmail.com>
2019-05-01 11:36:16 +0300

    finaal

--- WebApplication1/js/tab.js
+++ WebApplication1/js/tab.js
@@ -1,4 +1,4 @@
-﻿function openCity(evt, cityName) {
+﻿function tapOpen(evt, tapName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -8,6 +8,6 @@
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tapName).style.display = "block";
     evt.currentTarget.className += " active";
 }
